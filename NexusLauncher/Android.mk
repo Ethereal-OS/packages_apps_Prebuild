144585660dba8e46bba75605f32396d715f8927f
IamCOD3X <sourabh.panchal28@gmail.com>
2023-12-29 17:47:10 +0530

    fix

---
+++ NexusLauncher/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := NexusLauncher
+
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+
+LOCAL_MODULE_CLASS := APPS
+
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+
+LOCAL_CERTIFICATE := PRESIGNED
+
+LOCAL_OVERRIDES_PACKAGES := Launcher3 Launcher3QuickStep TrebuchetQuickStep QuickStep
+
+include $(BUILD_PREBUILT)
